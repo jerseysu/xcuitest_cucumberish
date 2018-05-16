37f19cc292e08481bcfcd51e981b00da898d46de
jerseysu <loverjersey@gmail.com>
2018-05-16 17:47:22 +0800

    Update: gherkin parser detail

--- FoodTrackerUITests/Tools/gherkin_parser.rb
+++ FoodTrackerUITests/Tools/gherkin_parser.rb
@@ -3,10 +3,9 @@ require 'gherkin/parser'
 require 'gherkin/pickles/compiler'
 require 'json'
 parser = Gherkin::Parser.new
-file = File.new("a.feature", "r")
+file = File.new("../Features/food.feature", "r")
 gherkin_document = parser.parse(file.read)
 pickles = Gherkin::Pickles::Compiler.new.compile(gherkin_document)
-pickles.each do |a|
-#puts JSON.pretty_generate(pickles.first)
-puts JSON.pretty_generate(a)
+pickles.each do |scenarios|
+puts JSON.pretty_generate(scenarios)
 end
